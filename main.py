05e4f7450555929b342b5810c9b2436476544461
YeniferZapata10 <yeniferzapata107@gmail.com>
2022-11-17 15:10:49 -0500

    Creación de urls

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from flask_jwt_extended import create_access_token, verify_jwt_in_request
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
+import re
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -35,6 +36,47 @@ def create_token():
         return jsonify({"token":access_token, "user_id":user["_id"]})
     else:
         return jsonify({"msg":"Correo o contraseña incorrecta"}), 401
+
+@app.before_request
+def before_request_callback():
+    endpoint = limpiarUrl(request.path)
+    rutaExcluida = ["/login"]
+    if rutaExcluida.__contains__(request.path):
+        print("Ruta excluida"+str(request.path))
+        pass
+    elif verify_jwt_in_request():
+        usuario = get_jwt_identity()
+        if usuario['rol'] is not None:
+            permiso = validarPermiso(endpoint, request.method, usuario['rol']['_id'])
+            if not permiso:
+                return jsonify({"mensaje":"No tiene permisos para ejecutar esta acción"}), 401
+            else:
+                return jsonify({"mensaje":"Puede ejecutar la acción"}), 200
+
+def validarPermiso(endpoint, metodo, id_rol):
+    url = data["url-ms-seguridad"]+'validar-permiso/rol/'+id_rol
+    tienePermiso = False
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    body ={
+        "url": endpoint,
+        "metodo": metodo
+    }
+    respuesta = requests.get(url, json=body, headers=headers)
+    try:
+        datos = respuesta.json()
+        if "_id" in datos:
+            tienePermiso = True
+    except:
+        pass
+    return tienePermiso
+
+def limpiarUrl(url):
+    partesUrl = url.split('/')
+    for parte in partesUrl:
+        if re.search('\\d', parte):
+            url = url.replace(parte, '?')
+    return url
+
 
 @app.route("/candidatos",methods=['POST'])
 def createC():
@@ -72,6 +114,149 @@ def DeleteC(id):
     url = data["url-ms-backend"] + '/candidatos/'+id
     respuesta = requests.delete(url, headers=headers)
     return jsonify(respuesta.json())
+
+@app.route("/candidatos/<string:id>/partidos/<string:id_partido>", methods=['PUT'])
+def asignarPartido(id, id_partido):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/candidatos/' + id + '/partidos/'+id_partido
+    respuesta = requests.put(url, headers=headers)
+    return jsonify(respuesta.json())
+
+#MESAS
+@app.route("/mesas",methods=['POST'])
+def createM():
+    datosMesas = request.get_json()
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/mesas'
+    respuesta = requests.delete(url, json=datosMesas, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/mesas/<string:id>",methods=['GET'])
+def getOneM(id):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/mesas/' + id
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/mesas",methods=['GET'])
+def getAllM():
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/mesas'
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/mesas/<string:id>",methods=['PUT'])
+def updateM(id):
+    datosM = request.get_json()
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/mesas/' + id
+    respuesta = requests.put(url, json=datosM, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/mesas/<string:id>",methods=['DELETE'])
+def deleteM(id):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/mesas/' + id
+    respuesta = requests.delete(url, headers=headers)
+    return jsonify(respuesta.json())
+
+#PARTIDOS
+@app.route("/partidos",methods=['POST'])
+def createP():
+    datosMesas = request.get_json()
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/partidos'
+    respuesta = requests.delete(url, json=datosMesas, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/partidos/<string:id>",methods=['GET'])
+def getOneP(id):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/partidos/' + id
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/partidos",methods=['GET'])
+def getAllP():
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/partidos'
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/partidos/<string:id>",methods=['PUT'])
+def updateP(id):
+    datosP = request.get_json()
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/partidos/' + id
+    respuesta = requests.put(url, json=datosP, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/partidos/<string:id>",methods=['DELETE'])
+def deleteP(id):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/partidos/' + id
+    respuesta = requests.delete(url, headers=headers)
+    return jsonify(respuesta.json())
+
+#RESULTADOS
+@app.route("/resultados/candidato/<string:id_candidato>/mesa/<string:id_mesa>",methods=['POST'])
+def createR(id_candidato, id_mesa):
+    datosR = request.get_json()
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/resultados/candidato/'+id_candidato+'/mesa/'+id_mesa
+    respuesta = requests.delete(url, json=datosR, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/resultados/<string:id>",methods=['GET'])
+def getOneR(id):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/resultados/' + id
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/resultados", methods=['GET'])
+def getAllR():
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/resultados'
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/resultados/<string:id>/candidato/<string:id_candidato>/mesa/<string:id_mesa>",methods=['PUT'])
+def updateR(id, id_candidato, id_mesa):
+    datosR = request.get_json()
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/resultados/' + id+'/candidato/'+id_candidato+'/mesa/'+id_mesa
+    respuesta = requests.put(url, json=datosR, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/resultados/<string:id>",methods=['DELETE'])
+def deleteR(id):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/resultados/' + id
+    respuesta = requests.delete(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/reportes/candidato/<string:id_candidato>", methods=['GET'])
+def resultados(id_candidato):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/reportes/candidato/' + id_candidato
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/reportes/resultado_mayor", methods=['GET'])
+def getResulMayor():
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/reportes/resultado_mayor'
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
+@app.route("/reportes/promedio/candidato/<string:id_candidato>", methods=['GET'])
+def promedioCandidatos(id_candidato):
+    headers = {"Content-Type": "application/json; charset = utf-8"}
+    url = data["url-ms-backend"] + '/reportes/promedio/candidato/' + id_candidato
+    respuesta = requests.get(url, headers=headers)
+    return jsonify(respuesta.json())
+
 
 def loadFileConfig():
     with open('config.json') as f:
